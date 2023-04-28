6a53fb92bc9c4fe7e6a4508a2b08c2732909e815
priyanakumarisahu <priyankakumarisahu95@gmail.com>
2023-04-28 18:48:38 +0530

    1st commit

---
+++ jenkinproject/src/test/java/jenkinproject/sample.java
@@ -0,0 +1,14 @@
+package jenkinproject;
+
+import org.testng.annotations.Test;
+
+public class sample {
+	public class SampleTest {
+		@Test
+		public void test() {
+			System.out.println("method running");
+		}
+
+	}
+
+}
